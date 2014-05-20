9d6e4c902de5b01177f1bc5ecb964ecf247be763
Perevertun V.M. <vitalik.perevertun@gmail.com>
2014-05-20 22:30:49 +0300

    * ChannelStatusCmd: add channel status agi cmd;

---
+++ root_src/src/org/ast4j/agi/cmd/ChannelStatusCmd.java
@@ -0,0 +1,31 @@
+package org.ast4j.agi.cmd;
+
+public class ChannelStatusCmd extends BaseAgiCmd
+{
+	/**
+     * The name of the channel to query or <code>null</code> for the current
+     * channel.
+     */
+    private final String channel;
+	
+    public ChannelStatusCmd()
+	{
+    	this(null);
+	}
+    
+    public ChannelStatusCmd(String channel)
+	{
+		this.channel = channel;
+	}
+
+	public String getChannel()
+	{
+		return channel;
+	}
+
+	@Override
+	public String buildCmd()
+	{
+		return "CHANNEL STATUS" + (channel == null ? "" : " " + escapeAndQuote(channel));
+	}
+}
