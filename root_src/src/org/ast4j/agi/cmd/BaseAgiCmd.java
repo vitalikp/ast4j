704946ba618a585acdf8630592132b5942069649
Perevertun V.M. <vitalik.perevertun@gmail.com>
2014-05-20 22:26:21 +0300

    * BaseAgiCmd: add base agi cmd class;

---
+++ root_src/src/org/ast4j/agi/cmd/BaseAgiCmd.java
@@ -0,0 +1,26 @@
+package org.ast4j.agi.cmd;
+
+public abstract class BaseAgiCmd implements IAgiCmd
+{
+	/**
+     * Escapes and quotes a given String according to the rules set by
+     * Asterisk's AGI.
+     * 
+     * @param s the String to escape and quote
+     * @return the transformed String
+     */
+    protected String escapeAndQuote(String s)
+    {
+        String tmp;
+
+        if (s == null)
+        {
+            return "\"\"";
+        }
+
+        tmp = s;
+        tmp = tmp.replaceAll("\\\"", "\\\\\""); // escape quotes
+        tmp = tmp.replaceAll("\\\n", ""); // filter newline
+        return "\"" + tmp + "\""; // add quotes
+    }
+}
