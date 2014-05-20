6ad45bb49f54dc4ec4d1bdf123a92fff5c43a4dd
Perevertun V.M. <vitalik.perevertun@gmail.com>
2014-05-20 22:33:30 +0300

    * VerboseCmd: add verbose agi cmd;

---
+++ root_src/src/org/ast4j/agi/cmd/VerboseCmd.java
@@ -0,0 +1,47 @@
+package org.ast4j.agi.cmd;
+
+public class VerboseCmd extends BaseAgiCmd
+{
+	/**
+     * The message to send.
+     */
+    private String message;
+
+    /**
+     * The verbosity level to use.<p>
+     * Must be in [1..4]
+     */
+    private int level;
+	
+	public VerboseCmd(String message, int level)
+	{
+		this.message = message;
+		this.level = level;
+	}
+
+	public String getMessage()
+	{
+		return message;
+	}
+
+	public void setMessage(String message)
+	{
+		this.message = message;
+	}
+
+	public int getLevel()
+	{
+		return level;
+	}
+
+	public void setLevel(int level)
+	{
+		this.level = level;
+	}
+
+	@Override
+	public String buildCmd()
+	{
+		return "VERBOSE " + escapeAndQuote(message) + " " + level;
+	}
+}
