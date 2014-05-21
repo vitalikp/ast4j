e89e8eb0b53cf7f020c1024573ec4ac95d8a418e
Perevertun V.M. <vitalik.perevertun@gmail.com>
2014-05-21 21:35:31 +0300

    * IAgiScript: add agi script interface;

---
+++ root_src/src/org/ast4j/agi/IAgiScript.java
@@ -0,0 +1,19 @@
+package org.ast4j.agi;
+
+import java.io.IOException;
+
+public interface IAgiScript
+{
+	/**
+     * The service method is called by the AsteriskServer whenever this
+     * AgiScript should handle an incoming AgiRequest.
+     * 
+     * @param request the initial data received from Asterisk when requesting
+     *            this script.
+     * @param channel a handle to communicate with Asterisk such as sending
+     *            commands to the channel sending the request.
+     * 
+     * @throws IOException any exception thrown by your script will be logged.
+     */
+    void service(IAgiChannel channel) throws IOException;
+}
