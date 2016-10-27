b358dc975f58caf59b82cedcf0f25c589adffb01
Perevertun V.M. <vitalik.perevertun@gmail.com>
2016-10-27 14:46:56 +0300

    * AgiScript: rename method service to run;

--- root_src/src/org/ast4j/agi/AgiScript.java
+++ root_src/src/org/ast4j/agi/AgiScript.java
@@ -5,15 +5,15 @@ import java.io.IOException;
 public interface AgiScript
 {
 	/**
-     * The service method is called by the AsteriskServer whenever this
-     * AgiScript should handle an incoming AgiRequest.
-     * 
-     * @param request the initial data received from Asterisk when requesting
-     *            this script.
-     * @param channel a handle to communicate with Asterisk such as sending
-     *            commands to the channel sending the request.
-     * 
-     * @throws IOException any exception thrown by your script will be logged.
-     */
-    void service(AgiChannel channel) throws IOException;
+	 * The run method is called by the AsteriskServer whenever this
+	 * AgiScript should handle an incoming AgiRequest.
+	 *
+	 * @param request the initial data received from Asterisk when requesting
+	 *            this script.
+	 * @param channel a handle to communicate with Asterisk such as sending
+	 *            commands to the channel sending the request.
+	 *
+	 * @throws IOException any exception thrown by your script will be logged.
+	 */
+	void run(AgiChannel channel) throws IOException;
 }
